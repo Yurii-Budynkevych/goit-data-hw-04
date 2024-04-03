b6a0a1d339f3aa0b3f738fb50fd3e5d63de86256
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2024-04-03 17:06:15 +0300

    some math

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+a = np.array([1, 2, 3, 4, 5], dtype=float)
+print(a + 2)  # [3. 4. 5. 6. 7.]
+print(a - 1)  # [0. 1. 2. 3. 4.]
+
+print("---------------------------------------")
+
+a = np.array([[1, 2, 3],
+              [0, 1, 2]])
+
+b = np.array([[ 1, 2],
+              [ 0, 4],
+              [-1, 0]])
+
+c = np.dot(a, b)
+print(c)
